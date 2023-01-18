3d0865a8f3d5f400d31f67174a55085aa56cba17
Omer Hamid Kamisli <me@ohkamisli.dev>
2023-01-18 13:39:55 -0800

    Creating card View & Displaying Data in a list

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,44 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Ömer Hamid Kamışlı on 1/18/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+                .accessibilityAddTraits(.isHeader)
+            
+            Spacer()
+            
+            HStack {
+                Label(String(scrum.attendees.count), systemImage: "person.3") .accessibilityLabel("\(scrum.attendees.count) attendees")
+                
+                Spacer()
+                
+                Label(String(scrum.lengthInMinutes), systemImage: "clock")
+                    .labelStyle(.trailingIcon)
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+            }
+            .font(.caption)
+        }
+        .foregroundColor(scrum.theme.accentColor)
+        
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
+
---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Ömer Hamid Kamışlı on 1/18/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
